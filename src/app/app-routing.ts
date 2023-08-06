9b87eb66d658dc85101605834f0f4c31d854905d
Pawan <paliwalpawan505@gmail.com>
2023-08-06 13:22:13 +0530

    added detail page and make responsive it

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PhonesComponent } from './phones/phones.component';
 import { BodyComponent } from './body/body.component';
+import { PhonesDetailComponent } from './phones/phones-detail/phones-detail.component';
 
 const router: Routes = [
   {path:'',component:BodyComponent,pathMatch:'full'},
-    {path:'phones',component:PhonesComponent}
+    {path:'phones',component:PhonesComponent},
+    {path:'phones/detail',component:PhonesDetailComponent}
 ];
 
 @NgModule({
